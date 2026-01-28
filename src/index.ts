caebf34a8bf8acfa54e20b26b419891c7e0dfbd7
AbinashBasnet5 <basnetabinash27@gmail.com>
2026-01-28 14:12:50 +0545

    initial setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hellow World");
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.get("/",(req, res) =>  {
+     res.json({status: "Api running perfectly"})
+});
+
+const PORT = 8000;
+
+app.listen(PORT, () =>  {
+     console.log("Backend running in the port", PORT);
+})
