abd3fcc9a5f430db1bf13b0b3d729b200f7ca826
AbinashBasnet5 <basnetabinash27@gmail.com>
2026-02-01 13:46:40 +0545

    feat (auth) : added the feature of registering the user

---
+++ src/utils/jwt.ts
@@ -0,0 +1,21 @@
+import jwt, { type JwtPayload } from "jsonwebtoken";
+
+
+const accessToken = process.env.ACCESS_TOKEN_SECRET;
+if (!accessToken) {
+    throw new Error("ACCESS_TOKEN_SECRET is not defined");
+}
+
+export const generateAccessToken = (userId: string, email: string) => {
+    return jwt.sign({ userId, email }, accessToken!, { algorithm: "HS256", expiresIn: "15m" });
+}
+
+export const verifyAccessToken = (token: string) => {
+    try {
+        const decoded = jwt.verify(token, accessToken!) as JwtPayload;
+        return decoded;
+    } catch (error: any) {
+        console.log("token verification faile ", error.message);
+        return null;
+    }
+}
---
+++ src/utils/loginSchema.ts
@@ -0,0 +1,9 @@
+import z from "zod";
+
+export const loginSchema = z.object({
+    email: z.string().email().trim().toLowerCase(),
+    password: z.string().min(8, "Password must be at least 8 characters long").trim(),
+}).strict();
+
+export type LoginSchema = z.infer<typeof loginSchema>;
+
